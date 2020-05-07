dedd49ed071cf88319738589bd4c5ce0efa64a54
Mike Freeman <mfreeman@greenshades.com>
2020-05-07 08:37:11 -0500

    Refactor for exclusively representing a iframed login

--- greenemployee-pta/src/app/app.component.ts
+++ greenemployee-pta/src/app/app.component.ts
@@ -9,27 +9,31 @@ import { SingleSignOnService } from './data/single-sign-on.service';
 export class AppComponent {
   title = 'app';
   alerts = [];
-  currentUrl = 'https://freemanland.greenemployee.com/default.aspx?s=3EE20554-FAD6-446C-BC52-E7BB87DD955C';
+  currentUrl = '';
 
   constructor(
     private singleSignOnService: SingleSignOnService) {
   }
 
-  login() {
-    const authKey = '472BB613-FD3D-405F-AB46-4BCCFA9F246D';
-    const parentCompanyID = 43277;
-    const companyID = 'MFREE';
-    const employeeID = 'AFHEW001';
-    const targetPortal = 'GreenEmployee';
-    const ipAddress = '24.217.186.188';
-    // tslint:disable-next-line: max-line-length
-    const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36';
+  // login() {
+  //   const authKey = '472BB613-FD3D-405F-AB46-4BCCFA9F246D';
+  //   const parentCompanyID = 43277;
+  //   const companyID = 'MFREE';
+  //   const employeeID = 'AFHEW001';
+  //   const targetPortal = 'GreenEmployee';
+  //   const ipAddress = '24.217.186.188';
+  //   // tslint:disable-next-line: max-line-length
+  //   const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36';
 
-    this.singleSignOnService.authenticateEmployee3(authKey, parentCompanyID, companyID,
-                                                   employeeID, targetPortal, ipAddress, userAgent)
-        .subscribe(url => {
-          console.log(url);
-          this.currentUrl = url;
-        });
+  //   this.singleSignOnService.authenticateEmployee3(authKey, parentCompanyID, companyID,
+  //                                                  employeeID, targetPortal, ipAddress, userAgent)
+  //       .subscribe(url => {
+  //         console.log(url);
+  //         this.currentUrl = url;
+  //       });
+  // }
+
+  login(subdomain: string, sessionGuid: string) {
+    this.currentUrl = 'https://' + subdomain + '.greenemployee.com/Default.aspx?s=' + sessionGuid;
   }
 }
